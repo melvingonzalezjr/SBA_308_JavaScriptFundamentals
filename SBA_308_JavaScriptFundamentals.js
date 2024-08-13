2c616b63fe763c3ae7c2af4ab7efceec5a63966d
melvingonzalezjr <mgonzalez713@gmail.com>
2024-08-12 23:50:20 -0500

    Create Getter Function for Due Assignments

--- SBA_308_JavaScriptFundamentals.js
+++ SBA_308_JavaScriptFundamentals.js
@@ -1,98 +1,110 @@
 // The provided course information.
 const CourseInfo = {
-    id: 451,
-    name: "Introduction to JavaScript"
-  };
-  
-  // The provided assignment group.
-  const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
-    assignments: [
-      {
-        id: 1,
-        name: "Declare a Variable",
-        due_at: "2023-01-25",
-        points_possible: 50
-      },
-      {
-        id: 2,
-        name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 150
-      },
-      {
-        id: 3,
-        name: "Code the World",
-        due_at: "3156-11-15",
-        points_possible: 500
-      }
-    ]
-  };
-  
-  // The provided learner submission data.
-  const LearnerSubmissions = [
-    {
-      learner_id: 125,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 47
-      }
-    },
-    {
-      learner_id: 125,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-02-12",
-        score: 150
-      }
-    },
-    {
-      learner_id: 125,
-      assignment_id: 3,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 400
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-24",
-        score: 39
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-03-07",
-        score: 140
-      }
-    }
-  ];
+  id: 451,
+  name: "Introduction to JavaScript",
+};
 
-  /*
-   * MY CODE BEGINS HERE
-   */
+// The provided assignment group.
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
+    {
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50,
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150,
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500,
+    },
+  ],
+};
+
+// The provided learner submission data.
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140,
+    },
+  },
+];
+
+/*
+ * MY CODE BEGINS HERE
+ */
 
 /*STEP 1: Get Learner ID */
 function getLearnerID(submissionsArray) {
-    const learnerIdArray = [];
+  const learnerIdArray = [];
 
-    submissionsArray.map(function (object) {
-        if (!learnerIdArray.includes(object['learner_id'])) {
-            learnerIdArray.push(object['learner_id'])
-        }
-    });
+  submissionsArray.map(function (object) {
+    if (!learnerIdArray.includes(object["learner_id"])) {
+      learnerIdArray.push(object["learner_id"]);
+    }
+  });
 
-    return learnerIdArray;
+  return learnerIdArray;
 }
 // console.log(getLearnerID(LearnerSubmissions));
 
+/*STEP 2: Get Assignments Due */
+function getAssignmentsDue(assignmentGroup) {
+  dueAssignments = [];
+  for (assignment of assignmentGroup.assignments) {
+    if (assignment.due_at <= new Date().toISOString().slice(0,10)) {
+      dueAssignments.push(assignment);
+    }
+  }
+  return dueAssignments;
+}
+// console.log(getAssignmentsDue(AssignmentGroup));
 
+/*STEP 3: Get Grades abd Average for Due Assignments */
 
